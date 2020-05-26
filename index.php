8d781365b10519676fa6f5d605eabbba4687dc59
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-05-26 10:43:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<title>
+Smart Solution
+</title>
+<head>
+<meta name="viewport"content="width=device-width,initial-scale=1">
+<link rel="stylesheet"type="text/css"href="css/index.css"/>
+<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div id="title-page">
+    <h2>Home</h2>
+    </div>
+	</br>
+<img class="wave" src="img/10.jpg">
+  <div class="container">
+    <div class="img">
+	  <a href="Admin/login.php"><img class="home_bg"src="img/admin.jpg"></a>
+	  </div>
+	  <div class="login-container">
+	  <a href="client/login.php"><img class="home_bg"id="user"src="img/user.png"></a>	  
+       </div>
+	   </div>
+</body>
+</html>
