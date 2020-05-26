33d4284763e7f9b5611209e2a3770ae60319e13a
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-05-26 10:45:21 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,37 @@
+<html>
+<title></title>
+<head>
+<link rel="stylesheet" href="css/style.css">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+  <div class="nav">
+  <div id="title-page">
+    <h2>Home Automation</h2>
+    </div>
+ <div class="bar">
+<input type="checkbox" id="check">
+<label for="check">
+<i class="fas fa-times" id="cancel"></i>
+<i class="fas fa-bars" id="btn"></i>
+
+</label>
+<div class="sidebar">
+<header>My App</header>
+<ul>
+<li><a href=""><i class="fas fa-qrcode"></i>Dashboard</a></li>
+<li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
+<li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
+<li><a href="#"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+</ul>
+</div>
+</div>
+</div>
+</br>
+<img class="wave" src="img/user2.jpg">
+<div class="container">
+     <div class="message">
+      <h5>You Are Welcome:</h5>
+	  </div>
+</body>
+</html>
