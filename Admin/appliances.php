e74eda11a150d8188acd803ca59bed4358e5787b
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-05-26 10:48:07 +0530

    Add files via upload

---
+++ Admin/appliances.php
@@ -0,0 +1,28 @@
+<html>
+<title></title>
+<head>
+<meta name="viewport"content="width=device-width,initial-scale=1">
+<link rel="stylesheet"type="text/css"href="css/add.css"/>
+<link rel="stylesheet"type="text/css"href="css/frame.css"/>
+<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+</head>
+<body>
+<div id="title-page">
+    <h2>Home Automation</h2>
+    </div>
+	</br>
+
+  <img class="wave" src="img/wave.jpg">
+  <div class="container">
+   <div class="message">
+      <h5>Sucessfully Client Added....</h5>
+		 <br><br>
+		 </div>
+		 <div class="para">
+		 <p class="pp">Go To Homepage---><a href="homepage.php">click here</a></p>
+		 </div>
+		
+		 </div>
+</body>
+</html>
