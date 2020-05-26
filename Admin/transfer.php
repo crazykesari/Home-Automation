805a7278507cfcbcab2252013ba4b9b9e6705b88
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-05-26 11:05:02 +0530

    Add files via upload

---
+++ Admin/transfer.php
@@ -0,0 +1,44 @@
+<?php
+     session_start();
+	 $error='';
+	 if(isset($_POST['login']))
+	 {
+		 if(empty($_POST['uname']) || empty($_POST['pass']))
+		 {
+			 $error="Username or Password is require";
+		 }
+		 else
+		 {
+			 $username=$_POST['uname'];
+			 $pass=$_POST['pass'];
+			 $conn = mysql_connect("localhost","root","abc");
+			  $username=stripslashes($username);
+
+			  $pass=stripslashes($pass);
+			   $username = mysql_real_escape_string($username);
+			    $pass = mysql_real_escape_string($pass);
+			  
+          $db = mysql_select_db("homeautomation",$conn);
+		   $query = mysql_query("select Name,type from userdata where Password='$pass' AND Email='$username'");
+		   $rows = mysql_num_rows($query);
+		   $rt=mysql_fetch_assoc($query);
+		   if($rows==1)
+		   {
+			   $_SESSION['login_user']=$username;
+			  
+			   header("location:homepage.php");
+		   }
+			   else
+			   {
+				   $error="Username or Password is invalid";
+				   
+			   }
+			    
+		 }
+		 mysql_close($conn);
+	 }
+		 else
+		 {
+         }            
+
+?>
